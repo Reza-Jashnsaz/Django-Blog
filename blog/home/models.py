be03d15a033ac7daa3488d31c58c87a16f23d6b0
Reza <rezajashnsaz0011@gmail.com>
2023-02-20 19:59:21 +0330

    fix __str__

--- blog/home/models.py
+++ blog/home/models.py
@@ -8,13 +8,13 @@ class Post(models.Model):
   text = models.TextField()
   created_at = models.DateTimeField()
 
-  def __str___(self):
+  def __str__(self):
     return self.title
 
 
 class Comment(models.Model):
-  text = models.CharField(max_length = 250)
+  text = models.TextField()
   post_id = models.ForeignKey("Post" , on_delete=models.CASCADE)
 
-  def __str___(self):
+  def __str__(self):
     return self.id
